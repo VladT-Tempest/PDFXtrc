b600e5ef3deb5f4d65f86c33b46e35c12b5cd30c
Alejandro Aguilera Wilches <aaguile@hotmail.com>
2025-04-07 18:34:26 -0500

    verifica version

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import gradio as gr
+import sys
 
 def greet(name):
-    return "Hello " + name + "!!"
+    python_version = sys.version.split()[0]
+    
+    return f"Hello {name}!!\nPython version: {python_version}"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
